86365a5d73755ab8d0d243a8f85bac6b40e64156
Rohit Aba Gaware <96184429+RohitGaware15@users.noreply.github.com>
2026-05-28 18:57:45 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -62,12 +62,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'breathe_esg'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
